1df7e3f7036c430a1ccb80c2729334134fbe48a3
Rohan <rohanam2000@gmail.com>
2026-02-05 10:28:24 +0530

    Comment Updates

--- Problems_List/Image Text Generation/Image_Text_Extraction.js
+++ Problems_List/Image Text Generation/Image_Text_Extraction.js
@@ -1,12 +1,18 @@
+// Import the Tesseract module. Used mainly for OCR (Optical Character Recognition)
 const Tesseract = require('tesseract.js');
 
+// Call the .recognize method to extract image content pixel by pixel
 Tesseract.recognize(
     './image.png',
     'eng'
 )
+
+// After extraction print the extracted text.
 .then(({ data : {text}})=> {
     console.log("Extracted text : ", text);
 })
+
+// If no text, print the error message.
 .catch(err => {
     console.log("Error extracting text...");
 })
