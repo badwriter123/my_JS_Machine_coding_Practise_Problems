5f1fba30ab427b126ae1fcff35fd417298c79f58
Rohan <rohanam2000@gmail.com>
2026-02-20 11:23:17 +0530

    server push

---
+++ Problems_List/express_server_query_parameter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const products = [
+    { id: 1, name: "Apple iPhone" },
+    { id: 2, name: "Samsung Galaxy" },
+    { id: 3, name: "Google Pixel" },
+    { id: 4, name: "OnePlus Phone" },
+    { id: 5, name: "Nokia Phone" }
+];
+
