683dd1a693cf1955579c62e6b247415cad38d058
Rohan <rohanam2000@gmail.com>
2026-02-23 11:13:55 +0530

    Code corrected

---
+++ Path Modules/path_example.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+// Current file path
+console.log('Current File: ', __filename);
+console.log('Current Directory: ', __dirname);
+
+// Join Paths (Works across OS)
+const filePath = path.join(__dirname, 'data', 'users', 'profile.json');
+console.log('Joined Path: ', filePath);
+
+// Get file Extension
+console.log('Extension name: ', path.extname('profile.json'));
+
+// Get Filename
+console.log("File name: ", path.basename('profile.json'));
+
+// Get Directory name
+// console.log("Directory name: ", path.dirname(filePath));
+console.log('Directory name: ', path.dirname(filePath));
+
+// Parse Path into Object
+console.log("Parsed Path: ", path.parse(filePath));
